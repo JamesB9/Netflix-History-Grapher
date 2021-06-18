7f47f27ece900651822e32884019908f1554255b
J-B <57415099+JamesB9@users.noreply.github.com>
2021-06-18 20:10:57 +0100

    top level check boxes appear and a sample google graph

---
+++ script.js
@@ -0,0 +1,133 @@
+google.charts.load('current', {packages: ['corechart', 'line']}); // Load Google Charts
+
+/**
+drawChart taken from google charts documentation
+**/
+function drawChart() {
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'Day');
+  data.addColumn('number', 'Guardians of the Galaxy');
+  data.addColumn('number', 'The Avengers');
+  data.addColumn('number', 'Transformers: Age of Extinction');
+
+  data.addRows([
+    [1,  37.8, 80.8, 41.8],
+    [2,  30.9, 69.5, 32.4],
+    [3,  25.4,   57, 25.7],
+    [4,  11.7, 18.8, 10.5],
+    [5,  11.9, 17.6, 10.4],
+    [6,   8.8, 13.6,  7.7],
+    [7,   7.6, 12.3,  9.6],
+    [8,  12.3, 29.2, 10.6],
+    [9,  16.9, 42.9, 14.8],
+    [10, 12.8, 30.9, 11.6],
+    [11,  5.3,  7.9,  4.7],
+    [12,  6.6,  8.4,  5.2],
+    [13,  4.8,  6.3,  3.6],
+    [14,  4.2,  6.2,  3.4]
+  ]);
+
+  var options = {
+    chart: {
+      title: 'Box Office Earnings in First Two Weeks of Opening',
+      subtitle: 'in millions of dollars (USD)'
+    },
+    width: 900,
+    height: 500
+  };
+
+  var chart = new google.charts.Line(document.getElementById('lineGraph'));
+
+  chart.draw(data, google.charts.Line.convertOptions(options));
+}
+
+/**
+ON DOCUMENT READY
+**/
+$(document).ready(() => {
+    console.log("Document Ready");
+});
+
+/**
+Reads and returns contents of a file object
+**/
+function readFile(file){
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        // Read File Object and return file contents
+        reader.addEventListener('load', (event) => {
+            const fileContents = event.target.result;
+            resolve(fileContents);
+        });
+
+        reader.addEventListener('progress', (event) => {
+            if (event.loaded && event.total) {
+                const percent = (event.loaded / event.total) * 100;
+                console.log(`Progress: ${Math.round(percent)}`);
+            }
+        });
+
+        reader.readAsText(file);
+    });
+}
+
+function extractNetflixData(csvString, delimeter=','){
+    const netflixData = {};
+    const lines = csvString.split('\n');
+
+    for(var i = 1; i < lines.length; i++){
+        // Extract show and date
+        line = lines[i];
+        data = line.split(delimeter);
+        if(data.length < 2) continue;
+        fullTitle = data[0].replace("\"","");
+        date = data[1].replace("\"", "").slice(0, -1);
+
+        // Split into show:season::episode etc...
+        titles = fullTitle.split(":");
+        depth = netflixData;
+        for(var j = 0; j < titles.length; j++){
+            title = titles[j];
+            if(!depth[title]){
+                if(j == titles.length - 1){ // If final depth
+                    depth[title] = []; // Create list for dates
+                }else{
+                    depth[title] = {}; // Create JSON to store next title
+                }
+            }
+            depth = depth[title];
+
+            if(j == titles.length - 1){ // If final depth
+                depth.push(date);
+            }
+        }
+    }
+    return netflixData;
+}
+
+function createCheckboxes(netflixData){
+    $("#checkboxList").empty()
+    keys = Object.keys(netflixData);
+    console.log(keys[0])
+    depth = 0;
+    for(var i = 0; i< keys.length; i++){ // For each top level key
+        $("#checkboxList").append(
+            `<input type="checkbox" id="${keys[i]}" value="${keys[i]}">
+            <label for="${keys[i]}"> ${keys[i]}</label><br>`);
+    }
+}
+
+
+/**
+ON FILE SUBMIT BUTTON CLICKED
+**/
+$(document).on("click", '#fileSubmitBtn', async () => {
+    var file = $('#fileSelect').prop('files')[0];
+    const contents = await readFile(file);
+    const netflixData = extractNetflixData(contents);
+    createCheckboxes(netflixData);
+    console.log(netflixData);
+    drawChart();
+});
