0d880c62219ebc276ba5e364a0addd2fe1c1ae81
J-B <57415099+JamesB9@users.noreply.github.com>
2022-10-01 14:19:57 +0100

    Working Tooltips and zoom

--- netflix-history-grapher/src/components/EpisodeScatterGraph.js
+++ netflix-history-grapher/src/components/EpisodeScatterGraph.js
@@ -26,10 +26,6 @@ ChartJS.register(
     zoomPlugin
 );
 
-const PrettyPrintJson = ({ data }) => (<div><pre>{
-    JSON.stringify(data, null, 2)}</pre></div>);
-
-
 const options = {
     plugins: {
         legend: {
@@ -119,92 +115,11 @@ const graphData = {
         showLine: true
     }]
 }
-/*
-const graphData = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: [1,2,3],
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: [3,2,1],
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-*/
-export default function FileUpload() {
-    const [selectedFile, setSelectedFile] = useState();
+
+export default function EpisodeScatterGraph(props) {
     const [data, setData] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
     const [graph, setGraph] = useState(graphData);
 
-    const changeHandler = (event) => {
-        var file = event.target.files[0];
-        setSelectedFile(file);
-        setIsFilePicked(true);
-
-        const reader = new FileReader();
-        reader.addEventListener('load', (event) => {
-            const result = event.target.result.replace(/['"]+/g, '');
-            const historyData = {
-                film: {},
-                series: {}
-            }
-
-            result.split('\n').forEach((line, index) => {
-                if (index === 0 || line === "") return;
-                var [title, date] = line.split(",")
-                var seasonWords = ["Series", "Season", "Chapter", "Volume"]
-                var [day, month, year] = date.split("/")
-                var date = `${year}-${month}-${day}`
-
-                if (seasonWords.some(word => title.includes(word))) { // SERIES
-                    var names = title.split(':');
-
-                    // Find Episode Name
-                    var episodeName = "";
-                    var seasonNameIndex = -1;
-                    for (var i = names.length - 1; i >= 0; i--) {
-                        var name = names[i];
-                        if (seasonWords.some(word => name.includes(word)) && i != names.length - 1) {
-                            seasonNameIndex = i;
-                            break;
-                        }
-                        episodeName = name + episodeName;
-                    }
-
-                    if (seasonNameIndex == -1) {
-                        historyData.film[title] = date;
-                        return; // Continue
-                    }
-
-                    var seasonName = names[seasonNameIndex].trim()
-                    var seriesName = names.slice(0, seasonNameIndex).join(':').trim()
-
-                    // Find Season
-                    if (historyData.series.hasOwnProperty(seriesName)) {
-                        if (historyData.series[seriesName].hasOwnProperty(seasonName)) {
-                            historyData.series[seriesName][seasonName][episodeName] = date;
-                        } else {
-                            historyData.series[seriesName][seasonName] = { [episodeName]: date }
-                        }
-                    } else {
-                        historyData.series[seriesName] = { [seasonName]: { [episodeName]: date } }
-                    }
-
-                } else { // FILM
-                    historyData.film[title] = date;
-                }
-            })
-            console.log(historyData)
-
-            setData(historyData);
 
 
 
@@ -265,16 +180,6 @@ export default function FileUpload() {
     };
 
     return (
-        <>
-            <Form>
-                <Form.Group controlId="formFileLg" className="mb-3">
-                    <Form.Label>Large file input example</Form.Label>
-                    <Form.Control type="file" size="lg" onChange={changeHandler} accept=".csv" />
-                </Form.Group>
-            </Form>
-
-            <Scatter options={options} data={graph} test={"hi"}/>
-            <PrettyPrintJson data={data} />
-        </>
+        <Scatter options={options} data={graph} test={"hi"}/>
     );
 }
